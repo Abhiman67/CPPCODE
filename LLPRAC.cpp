dc763099cda87c12832507ca2b13c5fda25674a7
Abhiman67~ <ironmanavengers6789@gmail.com~>
2024-08-09 00:53:05 +0530

    linked lists

--- LLPRAC.cpp
+++ LLPRAC.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 
-class Node() {
-
+class Node {
+ 
     public:
     int data;
     Node*Next;
@@ -44,16 +44,103 @@ class list{
 }
 
 void inserathead(int val){
-    Node*newnode= new Node(val);
-    if(Head==NULL){
-        Head=Tail=newnode;
 
+    Node*newnode= new Node(val);
+
+    if(Head==NULL){
+
+        Head=Tail=newnode;
     }
     else {
+        newnode->Next=Head;
+        Head=newnode;
+
         
 
     }
 }
+
+void insertatTail(int val){
+
+    Node*newnode= new Node(val);
+
+    if(Head==NULL){
+        Head=Tail=NULL;
+
+    }
+    else {
+        Tail->Next=newnode;
+        Tail=newnode;
+
+    }
+}
+
+void Printll() {
+    Node*Temp=Head;
+
+    while (Temp!=NULL)
+    {    cout << Temp->data << "  ->  " ;
+        Temp=Temp->Next;
+   }
+    
+
+}
+
+
+void insertatpos(int val,int pos){
+    Node*newnode= new Node(val);
+    Node*temp = Head;
+    
+    if(pos==1){
+
+        inserathead(val);
+        return;
+    }
+    for(int i=0;i<pos-1;i++){
+        temp=temp->Next;
+    }
+    newnode->Next=temp->Next;
+    temp->Next=newnode;
+
+}
+void icdelete(int val){
+    Node*newnode= new Node (val);
+
+    if(Head!=NULL){
+        delete Head;
+
+    }
+
+}
+
+// void deletehead (int val) {
+
+//     Node*newnode= new Node(val) ;
+//     Node*temp=Head;
+
+//     Head=Head->Next;
+//     temp->Next=NULL;
+
+//     free(temp);
+
+    
+
+// }
+// void deleteback(int val){
+//     Node*newnode=new Node(val);
+//     Node*temp=Head;
+//     while(temp->Next->Next!=NULL){
+//         temp=temp->Next;
+
+//     }
+
+//     temp->Next=NULL;
+//     delete Tail;
+//     Tail=temp;
+
+
+// }
+
 
 
 };
@@ -61,9 +148,17 @@ void inserathead(int val){
 
 
 int main(){
+    list ll ;
 
-    list.ll;
-
-
-
+    ll.inserathead(10);
+    ll.inserathead(20);
+    ll.inserathead(30);
+    ll.insertatTail(10);
+    ll.insertatTail(20);
+    ll.insertatTail(30);
+    ll.insertatpos(196,1);
+    // ll.deletehead(10);
+    // ll.deleteback(10);
+    // ll.search(10,30);
+    ll.Printll();
 }
